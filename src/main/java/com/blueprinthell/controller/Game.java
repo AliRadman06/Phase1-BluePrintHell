b51bf51bb358dc1fcecd81d0d937c5d7dfdc0e60
Ali Radman <radmanali8490@gmail.com>
2025-04-27 12:19:50 +0430

    main menu view done

---
+++ src/main/java/com/blueprinthell/controller/Game.java
@@ -0,0 +1,33 @@
+package com.blueprinthell.controller;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.net.URL;
+
+public class Game extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // === حتماً این مسیر را با مسیر واقعی فایل FXML‌تان تطبیق دهید ===
+        URL fxmlUrl = getClass().getResource("/com/blueprinthell/hello-view.fxml");
+        if (fxmlUrl == null) {
+            throw new RuntimeException("FXML file not found: /com/blueprinthell/main-menu.fxml");
+        }
+
+        FXMLLoader loader = new FXMLLoader(fxmlUrl);
+        Parent root = loader.load();
+
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Blueprint Hell");
+        primaryStage.setFullScreen(true);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,18 @@
+module com.blueprinthell {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.blueprinthell to javafx.fxml;
+    opens com.blueprinthell.controller to javafx.fxml;
+    exports com.blueprinthell.controller;
+
+}
